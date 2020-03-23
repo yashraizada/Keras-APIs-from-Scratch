d25d9f87295bda10914dfe136b1f79f546225b54
Yash Raizada <62168574+yashraizada@users.noreply.github.com>
2020-03-23 13:23:33 +0530

    Adding AdaGrad and correcting SGD & NAG

--- learning_algorithms.py
+++ learning_algorithms.py
@@ -11,7 +11,7 @@ class SDG():
         if self.update == None:
             self.update = np.zeros(np.shape(weight))
         
-        self.update += self.momentum * self.update + self.learning_rate * gradient
+        self.update = self.momentum * self.update + self.learning_rate * gradient
         return weight - self.update
 
 # Nesterov Accelerated Gradient Descent
@@ -26,5 +26,20 @@ class NAG():
             self.update = np.zeros(np.shape(weight))
             
         weight_look_ahead = weight - self.momentum * self.update
-        self.update += self.momentum * self.update + self.learning_rate * gradient_function(weight_look_ahead)
+        self.update = self.momentum * self.update + self.learning_rate * gradient_function(weight_look_ahead)
         return weight - self.update
+
+class AdaGrad():
+    def __init__(self, learning_rate = 0.001):
+        self.learning_rate = learning_rate
+        self.gradient_mag = None
+        self.epsilon = 1e-10
+    
+    def update(self, weight, gradient):
+        if self.gradient_mag == None:
+            self.gradient_mag = np.zeros(np.shape(gradient))
+        
+        # accumulate magnitude of gradients
+        self.gradient_mag += np.square(gradient)
+        
+        return weight - self.learning_rate * gradient / np.sqrt(self.gradient_mag + self.epsilon)
