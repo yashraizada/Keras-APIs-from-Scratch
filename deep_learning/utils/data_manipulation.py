a34e74382ba386d94ceda85072d547ec1481c3d7
Yash Raizada <62168574+yashraizada@users.noreply.github.com>
2020-03-31 18:10:35 +0530

    Create data_manipulation.py

---
+++ deep_learning/utils/data_manipulation.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def batch_generator(X, y=None, batch_size=64):
+	num_records = X.shape[0]
+	for i in np.arange(0, num_records, batch_size):
+		start, end = i, min(i+batch_size, num_records)
+		if y is not None:
+			yield X[start: end], y[start: end]
+		else:
+			yield X[start: end]
