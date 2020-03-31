1407cb80d78d2786b6dcfa7bec42ab7eaf2da60d
Yash Raizada <62168574+yashraizada@users.noreply.github.com>
2020-03-31 18:12:45 +0530

    Update optimizers.py

--- deep_learning/optimizers.py
+++ deep_learning/optimizers.py
@@ -3,33 +3,33 @@
 import numpy as np
 
 # Stochastic Gradient Descent with/without Momentum
-class SDG():
-    def __init__(self, learning_rate = 0.001, momentum = 0):
-        self.learning_rate = learning_rate
+class SGD():
+    def __init__(self, momentum = 0):
+        # self.learning_rate = learning_rate
         self.momentum = momentum
-        self.update = None
-    
-    def update(self, weight, gradient):
-        if self.update == None:
-            self.update = np.zeros(np.shape(weight))
-        
-        self.update = self.momentum * self.update + self.learning_rate * gradient
-        return weight - self.update
+        self.gamma = None
+
+    def update(self, weight, gradient, learning_rate = 0.1):
+        if self.gamma is None:
+            self.gamma = np.zeros(np.shape(weight))
+
+        self.gamma = self.momentum * self.gamma + learning_rate * gradient
+        return weight - self.gamma
 
 # Nesterov Accelerated Gradient Descent
 class NAG():
     def __init__(self, learning_rate = 0.001, momentum = 0.2):
         self.learning_rate = learning_rate
         self.momentum = momentum
-        self.update = []
+        self.gamma = []
         
     def update(self, weight, gradient_function):
-        if self.update == None:
-            self.update = np.zeros(np.shape(weight))
+        if self.gamma == None:
+            self.gamma = np.zeros(np.shape(weight))
             
-        weight_look_ahead = weight - self.momentum * self.update
-        self.update = self.momentum * self.update + self.learning_rate * gradient_function(weight_look_ahead)
-        return weight - self.update
+        weight_look_ahead = weight - self.momentum * self.gamma
+        self.gamma = self.momentum * self.gamma + self.learning_rate * gradient_function(weight_look_ahead)
+        return weight - self.gamma
 
 class AdaGrad():
     def __init__(self, learning_rate = 0.001):
