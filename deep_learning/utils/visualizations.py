e3a46d365e0ffe6210c8a0c3bd746681931d0faf
Yash Raizada <62168574+yashraizada@users.noreply.github.com>
2020-04-04 09:42:39 +0530

    added validation plot

--- deep_learning/utils/visualizations.py
+++ deep_learning/utils/visualizations.py
@@ -6,8 +6,22 @@ class plot_curves():
 		if type_ == 'cost':
 			# Plot cost vs epoch
 			plt.plot(history['cost'])
-			plt.title('Model accuracy')
+			plt.title('Cost vs Epoch')
 			plt.ylabel('Cost')
 			plt.xlabel('Epoch')
 			plt.legend(['Cost'], loc='upper left')
+			plt.show()
+
+		if type_ == 'valid':
+			legend_list = []
+
+			for key in history.keys():
+				if key != 'cost':
+					plt.plot(history[key])
+					legend_list.append(key)
+
+			plt.title('Errors vs Epoch')
+			plt.ylabel('Errors')
+			plt.xlabel('Epoch')
+			plt.legend(legend_list, loc='upper left')
 			plt.show()
