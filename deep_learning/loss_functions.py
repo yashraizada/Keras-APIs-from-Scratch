3ea10c1b985eea4e170c938a536dd86068c413cb
Yash Raizada <62168574+yashraizada@users.noreply.github.com>
2020-04-07 19:41:01 +0530

    Update loss_functions.py

--- deep_learning/loss_functions.py
+++ deep_learning/loss_functions.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .activation_functions import Sigmoid
+from activations import Sigmoid
 
 class LossFunction():
 	def loss(self, y, y_hat):
@@ -9,17 +9,17 @@ class LossFunction():
 		return NotImplementedError()
 
 class SquaredErrorLoss(LossFunction):
-	def loss(self, y, y_hat):
+	def __call__(self, y, y_hat):
 		return np.square(y_hat - y) * 0.5
 
 	def gradient(self, y, y_hat):
 		return y_hat - y
 
 class CrossEntropyLoss(LossFunction):
-	def loss(self, y, y_hat):
-		epsilon = 1e-10
-		y_hat = np.clip(y_hat, epsilon, 1 - epsilon)
+	def __call__(self, y, y_hat):
+		epsilon = 1e-5
+		y_hat = np.clip(y_hat, 1e-10, 1 - 1e-10)
 		return -1 * (y * np.log(y_hat) + (1-y) * np.log(1-y_hat))
 
 	def gradient(self, y, y_hat):
-		return -(y / y_hat) + ((1-y) / (1-y_hat))
+		return -y/y_hat + (1-y) / (1-y_hat)
