8bd20961cbbb9d444d338f4ccfc0789cbc2b8255
Yash Raizada <62168574+yashraizada@users.noreply.github.com>
2020-04-07 19:56:06 +0530

    Update metrics.py

--- deep_learning/metrics.py
+++ deep_learning/metrics.py
@@ -4,33 +4,21 @@ class Metric():
 	pass
 
 class MAE(Metric):
-	def __init__(self, y_hat, y):
-		self.y_hat = y_hat
-		self.y = y_hat
-
-	def __call__(self):
+	def __call__(self, y_hat, y):
 		return np.mean(np.abs(y_hat - y))
 
 	def __str__(self):
 		return 'MAE'
 
 class MSE(Metric):
-	def __init__(self, y_hat, y):
-		self.y_hat = y_hat
-		self.y = y_hat
-
-	def __call__(self):
+	def __call__(self, y_hat, y):
 		return np.mean(np.square(y_hat - y))
 
 	def __str__(self):
 		return 'MSE'
 
 class RMSE(Metric):
-	def __init__(self, y_hat, y):
-		self.y_hat = y_hat
-		self.y = y_hat
-
-	def __call__(self):
+	def __call__(self, y_hat, y):
 		return np.sqrt(np.mean(np.square(y_hat - y)))
 
 	def __str__(self):
